f6d50436b6185d124789b2d6b05a6c1b7d13e026
niwei822 <cecily.li.822@gmail.com>
2023-08-19 22:16:04 -0700

    add templates and app.py

---
+++ app.py
@@ -0,0 +1,55 @@
+from flask import Flask, render_template, request, redirect
+from flask_sqlalchemy import SQLAlchemy
+import os
+project_dir = os.path.dirname(os.path.abspath(__file__))
+database_file = "sqlite:///{}".format(os.path.join(project_dir, "mydatabase.db"))
+
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = database_file
+db = SQLAlchemy(app)
+
+class Expense(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    date = db.Column(db.String(50), nullable=False)
+    expensename = db.Column(db.String(50), nullable=False)
+    amount = db.Column(db.Integer, nullable=False)
+    category = db.Column(db.String(50), nullable=False)
+    
+
+@app.route('/')
+def add():
+    return render_template("add.html")
+
+@app.route('/addexpense', methods=['POST'])
+def addexpense():
+    date = request.form['date']
+    expensename = request.form['expensename']
+    amount = request.form['amount']
+    category = request.form['category']
+    
+    expense = Expense(date=date, expensename=expensename, amount=amount, category=category)
+    db.session.add(expense)
+    db.session.commit()
+  
+    return redirect('/')
+
+@app.route('/expenses')
+def expenses():
+    expenses = Expense.query.all()
+    return render_template('expenses.html', expenses=expenses)
+    
+    
+
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+     with app.app_context():
+        db.create_all()
+        app.run(debug=True)
